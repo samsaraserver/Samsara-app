4c9c898ba9eb372973cd80dcdb846d4a1b26202a
MatthewHubbard123 <Matrat2.hubbard@gmail.com>
2025-09-12 00:42:02 +0200

    navigation
    Signed-off-by: MatthewHubbard123 <Matrat2.hubbard@gmail.com>

---
+++ app/src/main/java/com/termux/app/login_page.java
@@ -0,0 +1,22 @@
+package com.termux.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.termux.R;
+
+public class login_page extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_page);
+
+
+
+
+    }
+}
