fbe23a794bc7297e793b32b4fa6fc3a17be64f89
MatthewHubbard123 <Matrat2.hubbard@gmail.com>
2025-09-13 23:36:03 +0200

    before i loose my work for the 4th time i added text to the home page and checked it with android phone not scaled yet need to do that eventually
    Signed-off-by: MatthewHubbard123 <Matrat2.hubbard@gmail.com>

--- app/src/main/java/com/termux/app/home_page.java
+++ app/src/main/java/com/termux/app/home_page.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.text.Html;
 
 import com.termux.R;
 
@@ -14,6 +16,10 @@ public class home_page extends Activity {
         setContentView(R.layout.home_page);
 
         ImageButton startServerButton = findViewById(R.id.imageButton9);
+        TextView textView3 = findViewById(R.id.textView3);
+
+        // Apply HTML formatting to textView3 to render bold and underline
+        textView3.setText(Html.fromHtml(textView3.getText().toString()));
 
         startServerButton.setOnClickListener(view -> {
             Intent intent = new Intent(home_page.this, TermuxActivity.class);
