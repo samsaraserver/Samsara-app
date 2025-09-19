a4f2b8b9569e7e2a4ce99a89360045e00146a6b0
MatthewHubbard123 <Matrat2.hubbard@gmail.com>
2025-09-19 14:07:34 +0200

    profile design
    Signed-off-by: MatthewHubbard123 <Matrat2.hubbard@gmail.com>

--- app/src/main/java/com/termux/app/profile_page.java
+++ app/src/main/java/com/termux/app/profile_page.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.termux.R;
 
@@ -15,7 +16,7 @@ public class profile_page extends Activity {
 
         NavbarHelper.setupNavbar(this);
 
-        Button button2 = findViewById(R.id.button2);
+        ImageButton button2 = findViewById(R.id.SetupBiometricsBtn);
         button2.setOnClickListener(v -> {
             Intent intent = new Intent(profile_page.this, biometrics_page.class);
             startActivity(intent);
