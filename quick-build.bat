eb6c17048ac846f483df2b74f36603cfe3d10077
Max0xDay <backup.day28@gmail.com>
2025-11-14 12:51:01 +0200

    stopped quickbuild running a full sonarrscann

--- quick-build.bat
+++ quick-build.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Quick building SamsaraServer...
 set JAVA_HOME=C:\Program Files\Microsoft\jdk-17.0.16.8-hotspot
-./gradlew.bat installDebug -x lint --daemon
+./gradlew.bat installDebug -x lint -x sonarqube --daemon
 echo.
 echo SamsaraServer installed successfully!
