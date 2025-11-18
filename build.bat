bedb0a43c8ddf351ed4a682487761aaa99af03c2
Max0xDay <backup.day28@gmail.com>
2025-11-18 10:20:12 +0200

    build changes for stable relese building

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Building Samsara Server
 set JAVA_HOME=C:\Program Files\Microsoft\jdk-17.0.16.8-hotspot
-./gradlew.bat build
+./gradlew.bat build -x lint
 echo.
 echo SamsaraServer built
